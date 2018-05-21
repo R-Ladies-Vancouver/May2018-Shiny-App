149924f47ee2a3193f00fc0f45c43d7b205d1d17
Nazli Ozum Kafaee <nazliozum@gmail.com>
2018-05-20 23:37:24 -0700

    Create pres template based on RStudio tutorials

---
+++ shiny_example/app.R
@@ -0,0 +1,17 @@
+library(shiny) #load the package
+
+ui <- fluidPage(
+  sliderInput(inputId = "num",
+              label = "Choose a number",
+              value = 25, min = 1, max = 100),
+  
+  plotOutput("hist")
+  
+) # user interface
+
+server <- function(input, output) {
+  output$hist <- renderPlot({ hist(rnorm(input$num)) })
+  
+}
+
+shinyApp(ui = ui, server = server) # launch the app
